7262b2d6659844c854023ce6f47a114d2db381ca
Tyuneev <68205964+Tyuneev@users.noreply.github.com>
2020-12-07 21:48:35 +0300

    Initial Commit

---
+++ Shared/ChatRAFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatRAFTApp.swift
+//  Shared
+//
+//  Created by Алексей Тюнеев on 07.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ChatRAFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
